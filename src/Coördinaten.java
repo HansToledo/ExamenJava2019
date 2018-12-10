3c75f64546d9f37b625fae3a695b272dde1608a7
Hans <hansie00@hotmail.com>
2018-12-10 20:57:57 +0100

    Test Git

---
+++ src/Coördinaten.java
@@ -0,0 +1,15 @@
+public class Coördinaten {
+    private double breedte;
+    private double lengte;
+    //voorbeeld: 50.7545, 6.0211
+    public Coördinaten(double breedte, double lengte){
+//intialisatie van variabelen
+    }
+    //triviale methods
+    public double getBreedte(){
+//breedtegraden (latitude)
+    }
+    public double getLengte(){
+//lengtegraden (longitude)
+    }
+}
