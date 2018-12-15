05f8abaa164081d5df688567a4f7b91746387361
Hans <Hans.vandeweyer@outlook.com>
2018-12-15 14:35:19 +0100

    Java classe aangemaakt die de preparedstatements gaat bevatten om tegen de database af te vuren. Settings.properties hernoemd naar db.properties.

---
+++ src/database/DBqueries.java
@@ -0,0 +1,32 @@
+package database;
+
+/**
+ * @Autor: Hans Van De Weyer
+ * @Date: 15/12/2018
+ * @Project: Examen Januari 2019
+ * @Purpose: Klasse bevat alle preparedstatemens queries.
+ */
+
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBqueries {
+    private Connection dbConnection; // manages the connection
+    private PreparedStatement selectAllBoats;
+
+
+    public DBqueries(){
+        try {
+            dbConnection = database.DBConnection.getConnection();
+
+            // EXMAPLE: query that selects all boats entries
+            selectAllBoats = dbConnection.prepareStatement(
+                    "SELECT * FROM boats ORDER BY BoatName, Name");
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
