dadd1cc919d9314534ce1937ba46e8dffb525205
Peter <peter_raes@telenet.be>
2018-12-15 13:12:00 +0100

    CommitPeter15/12/2018 13.11

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -11,6 +11,64 @@ import com.sun.xml.internal.bind.v2.runtime.Coordinator;
  */
 public abstract class Actor {
 
-   // private Coordinaten coordinaten;
+    private Coördinaten locatie;
+    private double afstand;
+    private double snelheid;
+    private double reactieTijd;
+    private double wendbaarheid;
+    private double grootte;
+    private double capaciteit;
+    private int koers;
 
+    public Coördinaten getLocatie() {
+        return locatie;
+    }
+
+    public double getAfstand() {
+        return afstand;
+    }
+
+    public double getSnelheid() {
+        return snelheid;
+    }
+
+    public double getReactieTijd() {
+        return reactieTijd;
+    }
+
+    public double getWendbaarheid() {
+        return wendbaarheid;
+    }
+
+    public double getGrootte() {
+        return grootte;
+    }
+
+    public double getCapaciteit() {
+        return capaciteit;
+    }
+
+    public int getKoers() {
+        return koers;
+    }
+
+    public void setLocatie(Coördinaten locatie) {
+        this.locatie = locatie;
+    }
+
+    public void setSnelheid(double snelheid) {
+        this.snelheid = snelheid;
+    }
+
+    public void setGrootte(double grootte) {
+        this.grootte = grootte;
+    }
+
+    public void setCapaciteit(double capaciteit) {
+        this.capaciteit = capaciteit;
+    }
+
+    public void setKoers(int koers) {
+        this.koers = koers;
+    }
 }
