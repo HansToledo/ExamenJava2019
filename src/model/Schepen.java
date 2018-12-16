95b4964635880f6f4a7811482d82b7c980bf9fd4
Peter <peter_raes@telenet.be>
2018-12-16 19:43:06 +0100

    Schepen classe toegevoegd waardoor implementatie interface verander

---
+++ src/model/Schepen.java
@@ -0,0 +1,27 @@
+package model;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: peter<br/>
+ * Date: 16/12/2018<br/>
+ * Time: 19:37<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class Schepen extends Vaartuigen implements IStatusSubject {
+
+
+    @Override
+    public void addVerkeerstorenObserver(Verkeerstoren verkeerstorenObserver) {
+
+    }
+
+    @Override
+    public void removeVerkeerstorenObserver(Verkeerstoren verkeerstorenObserver) {
+
+    }
+
+    @Override
+    public void notifyVerkeerstorenObservers() {
+
+    }
+}
