036f806ac30b3665b2bcad13c6f68240921d1960
Peter <peter_raes@telenet.be>
2018-12-20 14:06:12 +0100

    alle huldiensten/verkeerstorens en schepen worden static bijgehouden in lijst actor class via factory lijsten kunnen we later nog nodig hebben

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -13,7 +13,7 @@ import java.util.List;
 public abstract class Actor  {
 
     public static ArrayList<Verkeerstoren> verkeerstorens = new ArrayList<Verkeerstoren>(); //via factory komen verkeerstorens in lijst
-    public static ArrayList<Schepen> schepenOpWater = new ArrayList<Schepen>();
+    public static ArrayList<Schepen> schepenOpWater = new ArrayList<Schepen>();             // via factory
     public static ArrayList<Vervoermiddel> mogelijkeHulpdiensten = new ArrayList<Vervoermiddel>();
 
     public void clearVerkeerstorenLijst(){
