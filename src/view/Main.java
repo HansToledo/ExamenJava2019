0d2fe4058877d63a69e0b441184c6e689932eb6c
Hans <Hans.vandeweyer@outlook.com>
2018-12-23 18:28:02 +0100

    gui

--- src/view/Main.java
+++ src/view/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/controller/sample.fxml"));
         primaryStage.setTitle("Examen2019");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
