646942c46231cb02eea87e1d505c70130eea4c26
Peter <peter_raes@telenet.be>
2018-12-25 11:50:55 +0100

    test directory aangemaakt voor automatisch genereren junit test

---
+++ test/model/ActorTest.java
@@ -0,0 +1,41 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: peter<br/>
+ * Date: 25/12/2018<br/>
+ * Time: 11:48<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class ActorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getHulpdienstStrategy() {
+    }
+
+    @Test
+    public void setHulpdienstStrategy() {
+    }
+
+    @Test
+    public void getAfstand() {
+    }
+
+    @Test
+    public void setAfstand() {
+    }
+
+    @Test
+    public void getLocatie() {
+    }
+
+}
---
+++ test/model/FactoryProducerTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: peter<br/>
+ * Date: 25/12/2018<br/>
+ * Time: 11:50<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class FactoryProducerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getFactory() {
+    }
+}
