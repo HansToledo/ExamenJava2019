7a3657f1575e6a654481c173000ee09c31519da3
Hans <Hans.vandeweyer@outlook.com>
2018-12-27 12:48:18 +0100

    .

---
+++ src/controller/RescueController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Vervoermiddel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RescueController {
+
+    public RescueController(KustwachtController parent, ObservableList<Vervoermiddel> schepenInNood){
+
+    }
+
+}
+
+
