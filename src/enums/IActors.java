8989ef3c48eff4f7d7c1d23581d2a1e8f9c93ded
Peter <peter_raes@telenet.be>
2018-12-28 13:52:09 +0100

    iactors

---
+++ src/enums/IActors.java
@@ -0,0 +1,11 @@
+package enums;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: peter<br/>
+ * Date: 28/12/2018<br/>
+ * Time: 13:47<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IActors {
+}
