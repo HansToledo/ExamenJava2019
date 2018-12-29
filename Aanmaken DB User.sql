2fe5150db48ab545b51966d0f2e1d53ea9dbd27f
Hans <Hans.vandeweyer@outlook.com>
2018-12-29 17:06:00 +0100

    .

--- Aanmaken DB User.sql
+++
@@ -1,5 +0,0 @@
-USE Kustwacht;
-CREATE USER 'commander'@'localhost' IDENTIFIED BY 'Zeeslag123';
-GRANT ALL PRIVILEGES ON *.* TO 'commander'@'localhost';
-ALTER USER 'commander'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Zeeslag123';
-flush privileges;
